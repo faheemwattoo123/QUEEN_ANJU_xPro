8d8341039a00939ed681872876079b6e818e17a5
faheemwattoo123 <faheemwattoowattoo157@gmail.com>
2025-02-01 16:52:41 +0500

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~CYtBHYgY#ueM9Vb0x2nLsClDklK5FK15BObqj35uSHWdrgcwHfhQ",
 PORT: process.env.PORT || "8000"
 };
